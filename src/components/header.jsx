5666eeb89125c97314befd320ef1f1f89b409b5e
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2023-05-05 14:21:20 +0300

    changes

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Heading (){
+    return <header>
+        <hi> Keeper</hi>
+     </header>
+}
+
+export  default Heading;
+
---
+++ src/components/note.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+
+
+function Note (){
+    return <div className="note">
+        <p>the heading of the page</p>
+        <p>the page content</p>
+    </div>
+}
+
+export default Note;
